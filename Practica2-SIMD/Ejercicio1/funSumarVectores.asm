adc0cf4e677986118cdd7ad8ac4e8bfdfc55793a
Luis Bustamante <lbustamante@dc.uba.ar>
2020-05-18 16:49:16 -0300

    Started Practica2

---
+++ Practica2-SIMD/Ejercicio1/funSumarVectores.asm
@@ -0,0 +1,21 @@
+global sumarVectores
+
+section .text
+sumarVectores:
+	push rbp
+	mov rbp, rsp
+	
+	;void SumarVectores(char *A, char *B, char *Resultado, int dimension);
+	; Puede asumir que la dimension de los vectores es de un tamaño multiplo 
+	; de la cantidad de elementos que procesa simultaneamente.
+	; 	rdi <- char *A
+	; 	rsi <- char *B
+	; 	rdx <- char *Resultado
+	; 	ecx <- dimension
+	
+	
+	 
+	
+	pop rbp
+	ret
+	
